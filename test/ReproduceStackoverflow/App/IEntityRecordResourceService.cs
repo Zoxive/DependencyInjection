a198557396b6faba345e388facee31eb0fee096b
Kyle Wascher <Kyle.Wascher@infor.com>
2018-01-03 09:43:49 -0500

    add comment above Lazy<T> circular ref

--- test/ReproduceStackoverflow/App/IEntityRecordResourceService.cs
+++ test/ReproduceStackoverflow/App/IEntityRecordResourceService.cs
@@ -9,6 +9,7 @@ public class EntityRecordResourceService : IEntityRecordResourceService
 {
 	public EntityRecordResourceService
 	(
+        // This is a circular dependency so its lazy, but even with it commented it the stackoverflow happens, i think its somewhere else
 		Lazy<UpdateResource> updateResource,
 		IEntityRecordResourceRepository entityRecordResourceRepository
 	)
